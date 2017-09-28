1dd7b16c01e23e10f34a8402f540d0631eeb25c8
wrycu <wrycu@users.noreply.github.com>
2017-09-28 08:00:33 -0400

    refactor(uuidIssue): clearly mark unknown UUID versions as such (#3)
    * refactor(uuidIssue): clearly mark unknown UUID versions as such
    
    * style(UuidIssue): use camelCase for variables
    
    * style(UuidIssue): remove unused variable

--- src/burp/UuidIssue.java
+++ src/burp/UuidIssue.java
@@ -19,37 +19,42 @@ public class UuidIssue implements IScanIssue {
 	}
 
 	@Override public String getIssueName() {
-		return "Version " + uuid.version() + " UUID in HTTP request";
+		int v = uuid.version();
+		if (v > 5 || v < 1)
+			return "Unknown version " + v + " UUID in HTTP Request";
+		else
+			return "Version " + v + " UUID in HTTP request";
 	}
 
 	@Override public String getIssueDetail() {
 		int v = uuid.version();
-		final String prefix = "The request contains the version " + v +
+		final String knownPrefix = "The request contains the version " + v +
 			" UUID <b>" + uuid + "</b> which is ";
 		switch (v) {
 			case 1:
 				StringBuilder mac = new StringBuilder(6 * 3);
 				for (long n = uuid.node(); n > 0; n >>= 8) mac.insert(0,
 						String.format(":%02X", n & 0xFF));
-				return prefix + "generated from <ul>" +
+				return knownPrefix + "generated from <ul>" +
 					"<li>the timestamp <b>" + new Date(getTimeFromUUID(uuid)) + "</b>,</li>" +
 					"<li>the clock sequence <b>" + uuid.clockSequence() + "</b> and</li>" +
 					"<li>the node (MAC address) <b>" + mac.substring(1) + "</b>.</li>" +
 					"</ul>This means that it's not fit for authorization purposes, as " +
 					"it can be easily regenerated once the node and the approximate time is known.";
 			case 2:
-				return prefix + "generated using the DCE algorithm from a " +
+				return knownPrefix + "generated using the DCE algorithm from a " +
 					"timestamp, a clock sequence, a domain ID and a node value. " +
 					"This means that it's not fit for authorization purposes, as " +
 					"it can be easily regenerated once the node and the approximate time is known.";
 			case 3:
-				return prefix + "derived from a name using MD5.";
+				return knownPrefix + "derived from a name using MD5.";
 			case 4:
-				return prefix + "randomly generated, although its entropy should be checked.";
+				return knownPrefix + "randomly generated, although its entropy should be checked.";
 			case 5:
-				return prefix + "derived from a name using SHA-1.";
+				return knownPrefix + "derived from a name using SHA-1.";
 			default:
-				return prefix + "generated/derived from an unknown data source.";
+				return "The request contains an undefined version " + v + " UUID <b>" + uuid +
+				    "</b> which is generated/derived from an <b>unknown data source</b>.";
 		}
 	}
 
